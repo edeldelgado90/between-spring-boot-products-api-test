03ba22b3fc8b600199ce3b305db9c6ed8c48cc1e
edelgado <edeldelgado90@gmail.com>
2025-03-11 20:45:03 +0100

    Add initial project setup with Spring Boot and pre-commit

---
+++ src/test/java/com/between/products/ProductsApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.between.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
