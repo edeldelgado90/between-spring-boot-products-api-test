66a0157746ff15861f24417b139a8cc26a088a9a
edelgado <edeldelgado90@gmail.com>
2025-03-12 22:56:13 +0100

    Set up test profile for integration tests

--- src/test/java/com/between/products/ProductsApiTestApplicationTests.java
+++ src/test/java/com/between/products/ProductsApiTestApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 class ProductsApiTestApplicationTests {
 
     @Test
@@ -19,6 +21,6 @@ class ProductsApiTestApplicationTests {
 
     @Test
     void mainMethodStartsApplication() {
-        ProductsApiTestApplication.main(new String[]{});
+        ProductsApiTestApplication.main(new String[]{"--spring.profiles.active=test"});
     }
 }
