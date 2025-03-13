39b9c6d5202b8d766b4450c5231cab374f98ba4b
edelgado <edeldelgado90@gmail.com>
2025-03-13 01:42:40 +0100

    Add Resilience4j circuit breaker for ProductService

--- src/main/java/com/between/products/adapter/out/rest/ProductService.java
+++ src/main/java/com/between/products/adapter/out/rest/ProductService.java
@@ -4,6 +4,7 @@ import com.between.products.domain.product.Product;
 import com.between.products.domain.product.ProductNotFoundException;
 import com.between.products.domain.product.ProductRequestException;
 import com.between.products.port.out.rest.ProductOutPort;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,6 +27,7 @@ public class ProductService implements ProductOutPort {
 
     @Override
     @Cacheable(value = "similarIds", key = "#productId")
+    @CircuitBreaker(name = "productService", fallbackMethod = "getProductSimilarIdsFallback")
     public Flux<Integer> getProductSimilarIds(Integer productId) {
         return webClient.get()
                 .uri("/product/{productId}/similarids", productId)
@@ -36,6 +38,7 @@ public class ProductService implements ProductOutPort {
 
     @Override
     @Cacheable(value = "productDetail", key = "#productId")
+    @CircuitBreaker(name = "productService", fallbackMethod = "getProductDetailFallback")
     public Mono<Product> getProductDetail(Integer productId) {
         return webClient.get()
                 .uri("/product/{productId}", productId)
@@ -43,6 +46,16 @@ public class ProductService implements ProductOutPort {
                 .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> handleError(new ProductNotFoundException(productId)))
                 .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> handleError(new ProductRequestException(productId, clientResponse.statusCode())))
                 .bodyToMono(Product.class);
+    }
+
+    public Flux<Integer> getProductSimilarIdsFallback(Integer productId, Throwable throwable) {
+        logger.warn("Fallback method called for getProductSimilarIds. Product ID: {}", productId, throwable);
+        return Flux.empty();
+    }
+
+    public Mono<Product> getProductDetailFallback(Integer productId, Throwable throwable) {
+        logger.warn("Fallback method called for getProductDetail. Product ID: {}", productId, throwable);
+        return Mono.empty();
     }
 
     private <T> Mono<T> handleError(Exception exception) {
